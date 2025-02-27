d02886e0c04116a2ca803e1bc9a1fe0ffa7a8f24
Nischal55 <nischalshakya55@gmail.com>
2025-02-27 13:42:34 +0545

    added entity wallets

--- src/main/java/models/Wallet.java
+++ src/main/java/models/Wallet.java
@@ -1,10 +1,17 @@
 package models;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="wallets")
 public class Wallet extends BaseEntity{
+    @Column(name="userId",unique=false, nullable = false)
     private Long userId;
+    
+    @Column(name="balance",unique = false, nullable = false)
     private double balance;
-
+    
+    //getters and setters
     public Long getUserId() {
         return userId;
     }
