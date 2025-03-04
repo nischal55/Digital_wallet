122638098facc305c3e09437dd79e9fc842fb727
Nischal55 <nischalshakya55@gmail.com>
2025-03-04 16:37:15 +0545

    Added test cases

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,64 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import controllers.WalletController;
+import controllers.LotteryTicketController;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    
+    private WalletController wc;
+    private LotteryTicketController lt;
+    
+    @BeforeEach
+    void setUp() {
+         wc = new WalletController();
+         lt = new LotteryTicketController();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    //Test for testing the load balance
+    @Test
+    public void load_balance() {
+      wc.balance = 10000;
+      wc.userId = 4l;
+      
+      boolean result = wc.loadBalance(wc);
+      assertTrue(result, "Balance should be loaded successfully");
+      
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    //Test for testing balance transfer
+    @Test
+    public void balance_transfer(){
+        Long userId = 4l;
+        String contact = "9841220813";
+        double amount = 100;
+        
+        boolean result = wc.balanceTransfer(userId, contact, amount);
+        assertTrue(result,"Balance Transfered successfully");
+        
     }
+    
+    
+    //Testing case for buying Tickets
+    @Test
+    public void buyLotteryTicket(){
+        lt.lottery_id = 4l;
+        lt.userId = 4l;
+        
+        boolean result = lt.buyLotteryTicket(lt);
+        assertTrue(result,"Ticket should be Purchased");
+    }
+   
+    
+    //Testing for finding Result of Lottery
+    @Test
+    public void findLotteryResult(){
+        Long lotteryId = 4l;
+        
+        int result = lt.findLotteryResult(lotteryId);
+        
+        assertTrue(result>0,"Lottery Result should be Generated");
+    }
+    
 }
